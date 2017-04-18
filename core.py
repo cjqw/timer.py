8bf245890bc148f47cce6cd1a2e88ead7dfd2200
cjqw <2462457751@qq.com>
2017-04-18 08:44:08 +0800

    write basic method in timer class

---
+++ core.py
@@ -0,0 +1,22 @@
+from time import time
+
+class Timer():
+    def __init__(self, * args):
+        self.timers = {}
+        for item in args:
+            self.timers.update({item:0})
+        self.records = []
+
+    def start(self,name):
+        self.records.append([name,time()])
+        return len(self.records) - 1
+
+    def end(self,idx):
+        name,start_time = self.records[idx]
+        time_cost = time() - start_time
+        self.timers[name] = self.timers[name] + time_cost
+        return time_cost
+
+    def output(self):
+        for name in self.timers:
+            print(name,'time:',self.timers[name],'s')
